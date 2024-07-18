4b9960c8f8ad7182c206a93e98979eb08a2bd9c2
anjaney <ithalpreetham09@gmail.com>
2024-07-18 10:29:07 +0530

    final commit

---
+++ src/main/java/IntroPrettham/Intropreethamacademy/App.java
@@ -0,0 +1,7 @@
+package IntroPrettham.Intropreethamacademy;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/IntroPrettham/Intropreethamacademy/ApiTest.java
@@ -0,0 +1,5 @@
+package IntroPrettham.Intropreethamacademy;
+
+public class ApiTest {
+
+}
