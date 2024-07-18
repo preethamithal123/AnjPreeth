47e25cc3e817c15f7823545ad865cd3e066ee09b
anjaney <ithalpreetham09@gmail.com>
2024-07-18 16:18:59 +0530

    simple change

--- src/test/java/IntroPrettham/Intropreethamacademy/Demo.java
+++ src/test/java/IntroPrettham/Intropreethamacademy/Demo.java
@@ -1,5 +1,21 @@
 package IntroPrettham.Intropreethamacademy;
 
+import org.testng.annotations.Test;
+
 public class Demo {
 
+	
+	
+	
+	@Test
+	public void small()
+	{
+	
+		System.out.println("small change");
+		
+		
+	}
 }
+
+
+
