0ef2a9b1401c908c775ddf7f121a2cb283e515ef
anjaney <ithalpreetham09@gmail.com>
2024-07-18 10:53:23 +0530

    anj commit

--- src/test/java/IntroPrettham/Intropreethamacademy/ApiTest.java
+++ src/test/java/IntroPrettham/Intropreethamacademy/ApiTest.java
@@ -1,5 +1,17 @@
 package IntroPrettham.Intropreethamacademy;
 
+import org.testng.annotations.Test;
+
 public class ApiTest {
+	
+	@Test
+	public void apiTest()
+	{
+		
+		System.out.println("git 1");
+		System.out.println("git 1");
+		System.out.println("git 1");
+		System.out.println("git 1");
+	}
 
 }
--- src/test/java/IntroPrettham/Intropreethamacademy/MavenTest.java
+++ src/test/java/IntroPrettham/Intropreethamacademy/MavenTest.java
@@ -1,5 +1,14 @@
 package IntroPrettham.Intropreethamacademy;
 
+import org.testng.annotations.Test;
+
 public class MavenTest {
+	
+	
+	@Test
+	public void webAuto()
+	{
+		System.out.println("first commit");
+	}
 
 }
